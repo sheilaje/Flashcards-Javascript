51919b5160a4aeb990db4077978f48e8ae97c0bc
Sheila Stephen and Tanvi Garg <sheila@>
2018-10-16 15:04:20 -0700

    create flashcards to learn JS and Jquery using bootstrap, JQuery DOM manipulation using click and toggle functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+  $("h2#clickJS").click(function(){
+    $("#showJS").slideToggle();
+  });
+  $("h2#clickOperators").click(function(){
+    $("#showOperators").slideToggle();
+  });
+});
