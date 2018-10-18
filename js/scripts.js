cd12e355431caff5de3a53515cac216a147872b3
Sheila Stephen <sheila.je@gmail.com>
2018-10-18 06:22:57 -0700

    make all the flashcards using slidetoggle() function

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,62 @@ $(document).ready(function(){
   $("h2#clickOperators").click(function(){
     $("#showOperators").slideToggle();
   });
+  $("h2#clickvariable").click(function(){
+    $("#showvariable").slideToggle();
+  });
+  $("h2#clickvc").click(function(){
+    $("#showvc").slideToggle();
+  });
+
+  $("h2#clickfunctions").click(function(){
+    $("#showfunctions").slideToggle();
+  });
+  $("h2#clickmethod").click(function(){
+    $("#showmethod").slideToggle();
+  });
+
+  $("h2#clickarguments").click(function(){
+    $("#showarguments").slideToggle();
+  });
+  $("h2#clickparameters").click(function(){
+    $("#showparameters").slideToggle();
+  });
+
+  $("h2#clickreturn").click(function(){
+    $("#showreturn").slideToggle();
+  });
+  $("h2#clickchainingmethods").click(function(){
+    $("#showchainingmethods").slideToggle();
+  });
+
+  $("h2#clickstrings").click(function(){
+    $("#showstrings").slideToggle();
+  });
+  $("h2#clickbooleans").click(function(){
+    $("#showbooleans").slideToggle();
+  });
+
+  $("h2#clickundefined").click(function(){
+    $("#showundefined").slideToggle();
+  });
+  $("h2#clickNaN").click(function(){
+    $("#showNaN").slideToggle();
+  });
+
+  $("h2#clickescape").click(function(){
+    $("#showescape").slideToggle();
+  });
+  $("h2#clickalert").click(function(){
+    $("#showalert").slideToggle();
+  });
+
+  $("h2#clickcomments").click(function(){
+    $("#showcomments").slideToggle();
+  });
+  $("h2#clickjquery").click(function(){
+    $("#showjquery").slideToggle();
+  });
+
+
+
 });
